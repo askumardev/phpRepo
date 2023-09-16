4b8c111ad5623ec2c6e4c667d200ffff356a235c
askumardev <askumar.dev@outlook.com>
2023-09-16 16:26:17 +0530

    index.php added

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+//php index.php
+//php -S localhost:8000 (to run in web browser)
+// echo 2 + 2; //semiocolon is mandatory in php
+
+$number = 4 + 5;
+echo $number;
+echo "\n";
+$str = 'satish';
+echo 'The name is ' . $str . " u entered." ;
+echo "\n";
+echo "The name is $str u entered";
