2c01a247a767503c84bf1a103bb82e7102153c49
askumardev <askumar.dev@outlook.com>
2023-09-16 16:53:15 +0530

    composer added and index php updated

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
 //php index.php
 //php -S localhost:8000 (to run in web browser)
-// echo 2 + 2; //semiocolon is mandatory in php
 
-$number = 4 + 5;
-echo $number;
-echo "\n";
-$str = 'satish';
-echo 'The name is ' . $str . " u entered." ;
-echo "\n";
-echo "The name is $str u entered";
+require __DIR__ . '/vendor/autoload.php';
+use Cocur\Slugify\Slugify;
+
+$slugify = new Slugify();
+echo $slugify->slugify('The sky is blue, and grass is green !!!');
