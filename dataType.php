18e111b188e7cbdbeef246236dfbd033f633252a
askumardev <askumar.dev@outlook.com>
2023-09-17 01:41:11 +0530

    math ex added

--- dataType.php
+++ dataType.php
@@ -9,8 +9,17 @@
 
   $x = 5985;
   var_dump($x); #The PHP var_dump() function returns the data type and value.
+  var_dump(is_int($x));
 
   $x = 10.365;
+  var_dump($x);
+  var_dump(is_int($x));
+  var_dump(is_float($x));
+
+  $x = 1.9e411;
+  var_dump($x);
+
+  $x = acos(8);
   var_dump($x);
 
   $cars = array("Volvo","BMW","Toyota");
---
+++ math.php
@@ -0,0 +1,20 @@
+<?php
+  # php math.php
+  echo(pi()); // returns 3.1415926535898
+  echo "\n";
+  echo(min(0, 150, 30, 20, -8, -200));  // returns -200
+  echo "\n";
+  echo(max(0, 150, 30, 20, -8, -200));  // returns 150
+  echo "\n";
+  echo(abs(-6.7));  // returns 6.7
+  echo "\n";
+  echo(sqrt(64));  // returns 8
+  echo "\n";
+  echo(round(0.60));  // returns 1
+  echo "\n";
+  echo(round(0.49));  // returns 0
+  echo "\n";
+  echo(rand());
+  echo "\n";
+  echo(rand(10, 100));
+?>
