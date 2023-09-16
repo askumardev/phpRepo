b6077fb7187e5d7f2e66b886d1781489acb18820
askumardev <askumar.dev@outlook.com>
2023-09-17 02:03:39 +0530

    arithimatic ex added

---
+++ arithimatic.php
@@ -0,0 +1,37 @@
+<?php
+//php arithimatic.php
+function add($x, $y){
+  return $x + $y;
+}
+
+function sub($x, $y){
+  return $x - $y;
+}
+
+function multiply($x, $y){
+  return $x * $y;
+}
+
+function divide($x, $y){
+  return $x * $y;
+}
+
+function mod($x, $y){
+  return $x % $y;
+}
+
+function expo($x, $y){
+  return $x ** $y;
+}
+
+echo add(20, 10);
+echo "\n";
+echo sub(20, 10);
+echo "\n";
+echo multiply(20, 10);
+echo "\n";
+echo divide(20, 10);
+echo "\n";
+echo mod(20, 10); # modulo division
+echo "\n";
+echo expo(20, 10); #Exponentiation
