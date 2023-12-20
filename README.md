163bf3c9e589990239453190aa028913f2e2e20b
askumardev <askumar.dev@outlook.com>
2023-12-21 00:52:35 +0530

    read me update

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 * To run a program use php file_name.php
 * //This is a comment in php
 * semiocolon is mandatory in php
-
 * composer create-project laravel/laravel firstApp
 * ps -ef | grep php
 * kill -9 25972
+* php -S localhost:8000
+* http://localhost:8000/bootstrapdemo.php
--- index.php
+++ index.php
@@ -1,6 +1,17 @@
-
-<?php
-  # php index.php
-
-  echo "Hello World\n";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php
+    # php -S localhost:8000
+    echo 'This is title';
+    ?></title>
+</head>
+<body>
+  <?php    
+    // File to be included in includeExample.php
+    echo "Hello World from index.php";
+  ?>
+</body>
+</html>
