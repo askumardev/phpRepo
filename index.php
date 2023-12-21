64cfd1fa37680d4d85e6d49483d11a7b5ca95729
askumardev <askumar.dev@outlook.com>
2023-12-21 21:56:00 +0530

    index update

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php
-    # php -S localhost:8000
-    echo 'This is title';
-    ?></title>
-</head>
-<body>
-  <?php    
-    // File to be included in includeExample.php
-    echo "Hello World from index.php";
-  ?>
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php
+      # php -S localhost:8000
+      echo 'This is title';
+      ?></title>
+  </head>
+  <body>
+    <?php    
+      // File to be included in includeExample.php
+      echo "Hello World from index.php";
+    ?>
+  </body>
 </html>
